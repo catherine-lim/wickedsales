241d6fa3b64152ebabdaebff6e2450689ca4782c
catherini168 <catherini168@gmail.com>
2019-08-21 19:16:59 +0000

    Define a stateless Header component in client/components/header.jsx that displays the store's name and logo. Add Header to App's render method

---
+++ client/components/header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2> Wicked Sales</h2>
+        <img src="/Images/WickedLogo.png" alt="Logo" />
+      </div>
+    );
+  }
+}
+export default Header;
