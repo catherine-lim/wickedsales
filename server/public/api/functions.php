5c6209dcc02f3a22b424e1a6a06a1b8979afb4e0
catherini168 <catherini168@gmail.com>
2019-08-22 22:08:00 +0000

    remove commented out code and test error in error handler

--- server/public/api/functions.php
+++ server/public/api/functions.php
@@ -4,7 +4,8 @@ function error_handler($error){
         "success" => "false",
         "error" => $error->getMessage(),
     );
-  
+    http_response_code(500);
+
     $json_output = json_encode($output);
     print($json_output);
 }
--- server/public/api/products.php
+++ server/public/api/products.php
@@ -1,21 +1,11 @@
 <?php
 
-// header('Content-Type: application/json');
-
-// if (empty($_GET['id'])) {
-//   readfile('dummy-products-list.json');
-// } else {
-//   readfile('dummy-product-details.json');
-// }
-
 require_once('./functions.php');
-
 set_exception_handler('error_handler');
-//http_response_code(500);
-//doStuff();
+
+doStuff();
 
 $output = file_get_contents('./dummy-products-list.json');
-
 print($output);
 
 
