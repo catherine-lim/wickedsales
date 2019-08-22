ea81a89850bd5d65e045bef625298369255a47dd
catherini168 <catherini168@gmail.com>
2019-08-22 19:19:24 +0000

    use file_get_contents to grab the contents of the dummy data , cause an error by calling a non-existant function, and notice the status code 500

--- server/public/api/functions.php
+++ server/public/api/functions.php
@@ -4,7 +4,7 @@ function error_handler($error){
         "success" => "false",
         "error" => $error->getMessage(),
     );
-    
+  
     $json_output = json_encode($output);
     print($json_output);
 }
--- server/public/api/products.php
+++ server/public/api/products.php
@@ -8,6 +8,14 @@
 //   readfile('dummy-product-details.json');
 // }
 
-require_once('functions.php');
+require_once('./functions.php');
+
+set_exception_handler('error_handler');
+
+
+$output = file_get_contents('./dummy-products-list.json');
+
+print($output);
+
 
 ?>
