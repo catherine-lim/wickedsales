d36e993365752eb3dbf95df15ffb89d502a25dd5
catherini168 <catherini168@gmail.com>
2019-08-23 00:25:57 +0000

    create connection file for mysql

--- server/public/api/functions.php
+++ server/public/api/functions.php
@@ -1,13 +1,13 @@
 <?php
+
 function error_handler($error){
     $output = array(
         "success" => "false",
-        "error" => $error->getMessage(),
+        "error" => $error->getMessage()
     );
     http_response_code(500);
 
     $json_output = json_encode($output);
     print($json_output);
 }
-
 ?>
--- server/public/api/products.php
+++ server/public/api/products.php
@@ -1,10 +1,14 @@
 <?php
-
 require_once('./functions.php');
+require_once('./db_connection.php');
+
 set_exception_handler('error_handler');
+
+if (!$conn){
+    throw new Exception("Connect failed: " . mysqli_connect_error());
+}
 
 $output = file_get_contents('./dummy-products-list.json');
 print($output);
-
 
 ?>
