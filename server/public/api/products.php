4dacd4e45282dcd94485bbdcd8651b4f0184bf80
catherini168 <catherini168@gmail.com>
2019-08-25 06:22:25 +0000

    change because too many errors check

--- server/public/api/products.php
+++ server/public/api/products.php
@@ -8,27 +8,30 @@ startup();
 require_once('./db_connection.php');
 
 if(empty($_GET['id'])){
- $whereClause = '';
-} else{
- $whereClause = "WHERE id = " . $_GET['id'];
-}
-
-if(!is_numeric($_GET['id'])){
-    throw new Exception("id needs to be a number");
+        $whereClause = '';
+} else if(!is_numeric($_GET['id'])){
+        throw new Exception("id needs to be a number");
 } else {
-    throw new Exception("invalid ID:" . ($_GET['id']));
+        $whereClause = "WHERE id = " . $_GET['id'];   
 }
 
 $query = "SELECT * FROM `products` " .$whereClause;
-
 $result = mysqli_query($conn, $query);
+
+if(!mysqli_num_rows($result)){
+throw new Exception("invalid ID:" . ($_GET['id']));
+}
+
 if(!$result){
     throw new Exception("Connect failed: " . mysqli_error());
 }
+
 $output = array();
 
 while ($row = mysqli_fetch_assoc($result)) {
  $output[] = $row;
 };
+
 print(json_encode($output));
+
 ?>
