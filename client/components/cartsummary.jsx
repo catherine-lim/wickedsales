b1f68b87ef153dd56810c93603bad2b851877d4a
catherini168 <catherini168@gmail.com>
2019-08-28 23:00:24 +0000

    Define a stateless CartSummaryItem Define a stateless CartSummary

---
+++ client/components/cartsummary.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './header';
+import CartSummaryItem from './cartsummaryitem';
+
+function CartSummary() {
+  return (
+    <React.Fragment>
+      <Header/>
+      <CartSummaryItem />
+
+    </React.Fragment>
+  );
+
+}
+export default CartSummary;
